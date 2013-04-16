01a9e91dcda1f49cffa60e67e0bfbe1c4f66539a
Scott Muc <scottmuc@gmail.com>
2013-04-17 03:43:33 +0530

    locking versions, need to figure out how to make platform depends in metadata.rb

--- Berksfile
+++ Berksfile
@@ -1,3 +1,4 @@
 site :opscode
 
 metadata
+
--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,6 @@ end
 recipe "go::server", "Installs and configures a Go server"
 recipe "go::agent", "Installs and configures a Go agent"
 
-depends "apt", "~> 1.9.0"
-depends "java", "~> 1.10.0"
+depends "apt", "1.9.2"
+depends "java", "1.10.0"
 
