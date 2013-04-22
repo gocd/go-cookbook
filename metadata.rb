be28f600245e5d899f448c63c8888031e65f00ae
Scott Muc <scottmuc@gmail.com>
2013-04-23 05:07:12 +0530

    adding test kitchen

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,7 @@ name             "go"
 description      "Installs/Configures Go servers and agents"
 version          "0.0.4"
 
-%w{ debian ubuntu}.each do |os|
-  supports os
-end
+supports "ubuntu" "12.04"
 
 recipe "go::server", "Installs and configures a Go server"
 recipe "go::agent", "Installs and configures a Go agent"
