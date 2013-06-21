05a80189f78116ebc7660cd4a0f7c9b1d4142c24
Tim Brown <tpbrown@gmail.com>
2013-06-21 10:43:09 -0700

    Don't search if :go :server attribute specified. Fix bad commit that checked for this.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "go"
 description      "Installs/Configures Go servers and agents"
-version          "0.0.7"
+version          "0.0.8"
 
 supports "ubuntu" "12.04"
 
--- recipes/agent.rb
+++ recipes/agent.rb
@@ -19,9 +19,9 @@ package "go-agent" do
 end
   
 if Chef::Config[:solo] || node.attribute.go?(:server)
-  Chef::Log.warn("Chef-solo invocation detected or node[:go][:server attribute specified].  node[:go][:server] attribute will be used for server instance configuration.")
+  Chef::Log.warn("Chef-solo invocation detected.  node[:go][:server] attribute will be used for server instance configuration.")
+  Chef::Log.info("Using #{node[:go][:server]} for server instance configuration, as specified in node[:go][:server].")
 else
-  if node
   go_servers = search(:node, "chef_environment:#{node.chef_environment} AND recipes:go-server")
   go_server = "#{go_servers[0][:ipaddress]}"
   go_server_autoregister = "#{go_servers[0][:go][:auto_register_agents]}"
