42b7707792c04f08bd804319610857d9fa47c22a
willejs <williamejsalt@gmail.com>
2013-11-22 11:24:13 +0000

    do not specify exact apt version
    specifying exact version causes conflicts with other cookbooks, apt shouldn't break with newer versions, so lets choose a version and above.

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,6 @@ recipe "go::server", "Installs and configures a Go server"
 recipe "go::agent", "Installs and configures a Go agent"
 recipe "go::default", "Installs and configures a Go server and agent on the same node"
 
-depends "apt", "1.9.2"
+depends "apt", ">= 1.9.2"
 depends "java", "1.10.0"
 
