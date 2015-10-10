fd733e2935734ec2a4848661373e3049593e450b
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-10 18:03:44 +0530

    Improve specs.

--- test/integration/default/serverspec/go_agent_daemon_spec.rb
+++ test/integration/default/serverspec/go_agent_daemon_spec.rb
@@ -3,18 +3,25 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe "Go Agent Daemon" do
-
-  it "has a running service of go-agent" do
-    expect(service("go-agent")).to be_running
+describe 'go agent package' do
+  it 'should be installed' do
+    expect(package('go-agent')).to be_installed
   end
 end
 
-describe "GO Agent Config" do 
-  describe file('/var/lib/go-agent/config/autoregister.properties') do 
-  	it {should be_file}
-  	it {should be_readable}
-  	it { should contain 'agent.auto.register.key=default_auto_registration_key' }
+describe 'go agent service' do
+  it 'should be running' do
+    expect(service('go-agent')).to be_running
+    expect(service('go-agent')).to be_enabled
   end
+end
 
+describe '/etc/default/go-agent' do
+  it 'should have the correct ownership and mode' do
+    expect(file('/etc/default/go-agent')).to exist
+    expect(file('/etc/default/go-agent')).to be_readable
+    expect(file('/etc/default/go-agent')).to be_mode(644)
+    expect(file('/etc/default/go-agent')).to be_owned_by('root')
+    expect(file('/etc/default/go-agent')).to be_grouped_into('root')
+  end
 end
--- test/integration/default/serverspec/go_server_daemon_spec.rb
+++ test/integration/default/serverspec/go_server_daemon_spec.rb
@@ -3,14 +3,34 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe "Go Server Daemon" do
 
-  # it "is listening on port 8153" do
-  #   expect(port(8153)).to be_listening
-  # end
+describe 'go server package' do
+  it 'should be installed' do
+    expect(package('go-server')).to be_installed
+  end
+end
 
-  it "has a running service of go-server" do
-    expect(service("go-server")).to be_running
+describe 'go server service' do
+  it 'should be running' do
+    expect(service('go-server')).to be_running
+    expect(service('go-server')).to be_enabled
   end
 
+  it 'should be listening on port 8153' do
+    expect(port(8153)).to be_listening
+  end
+
+  it 'should be listening on port 8154' do
+    expect(port(8154)).to be_listening
+  end
+end
+
+describe '/etc/default/go-server' do
+  it 'should have the correct ownership and mode' do
+    expect(file('/etc/default/go-server')).to exist
+    expect(file('/etc/default/go-server')).to be_readable
+    expect(file('/etc/default/go-server')).to be_mode(644)
+    expect(file('/etc/default/go-server')).to be_owned_by('root')
+    expect(file('/etc/default/go-server')).to be_grouped_into('root')
+  end
 end
