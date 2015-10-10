7b2236973e2e00ad4e4ca672495c4a8460be8e08
Tomasz Setkowski <tom@ai-traders.com>
2015-10-10 21:57:35 +0000

    install bundler first

--- ci-setup.sh
+++ ci-setup.sh
@@ -3,4 +3,5 @@
 wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.9.0-1_amd64.deb -qc -O "${HOME}/.chefdk_0.9.0-1_amd64.deb"
 sudo dpkg -i "${HOME}/.chefdk_0.9.0-1_amd64.deb"
 eval "$(chef shell-init bash)"
+sudo $(which chef) gem install bundler
 sudo $(which chef) bundle install
