bc83aca0478caae97d8fb554eb9c43818b353e8e
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-10 12:50:19 +0530

    Fix chef warning.

--- ci-setup.sh
+++ ci-setup.sh
@@ -2,5 +2,5 @@
 
 wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.9.0-1_amd64.deb -qc
 sudo dpkg -i chefdk_0.9.0-1_amd64.deb
-export PATH=/opt/chefdk/embedded/bin:$PATH
+export PATH=/opt/chefdk/bin:/opt/chefdk/embedded/bin:$PATH
 sudo $(which chef) gem install kitchen-docker
