f99d37a52901ee980278814496fad3ac4b338903
Tomasz Setkowski <tom@ai-traders.com>
2015-12-10 23:13:28 +0000

    update chefdk to 0.10

--- ci-setup.sh
+++ ci-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.9.0-1_amd64.deb -qc -O "${HOME}/.chefdk_0.9.0-1_amd64.deb"
-sudo dpkg -i "${HOME}/.chefdk_0.9.0-1_amd64.deb"
+wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.10.0-1_amd64.deb -qc -O "${HOME}/.chefdk_0.10.0-1_amd64.deb"
+sudo dpkg -i "${HOME}/.chefdk_0.10.0-1_amd64.deb"
 eval "$(chef shell-init bash)"
 sudo $(which chef) gem install bundler
 sudo $(which chef) exec bundle install
