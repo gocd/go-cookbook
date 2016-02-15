33b1b733b039fb3cf0cbf9b2e7f646b085a2218c
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2016-02-15 10:03:36 +0530

    Bump version to 1.1.0 and update changelog

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "gocd"
 description      "Installs/Configures Go servers and agents"
-version          "1.0.0"
+version          "1.1.0"
 
 supports "ubuntu"
 supports "centos"
@@ -11,7 +11,9 @@ recipe "gocd::server", "Installs and configures a Go server"
 recipe "gocd::agent", "Installs and configures a Go agent"
 recipe "gocd::repository", "Installs the go yum/apt repository"
 recipe "gocd::agent_windows", "Installs and configures Windows Go agent"
+recipe "gocd::server_windows", "Installs and configures Windows Go server"
 recipe "gocd::agent_linux", "Install and configures Linux Go agent"
+recipe "gocd::server_linux", "Install and configures Linux Go server"
 
 depends "apt"
 depends "java"
