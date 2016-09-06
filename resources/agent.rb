9e0a0ed8693c3e6c10e9074b5988bfd92dbdb084
Tomasz Setkowski <tom@ai-traders.com>
2016-09-06 10:00:01 +0000

    rewrite resources to use new chef custom resource syntax

--- resources/agent.rb
+++ resources/agent.rb
@@ -1,20 +1,94 @@
-#Base Resource
 actions :create, :delete
 
 default_action :create if defined?(default_action)
 
-attribute :service_action, :kind_of => [ Symbol, Array ], :required => false, :default => [:enable,:start]
+property :service_action, :kind_of => [ Symbol, Array ], :required => false, :default => [:enable,:start]
 
-attribute :agent_name, :name_attribute => true, :kind_of => String, :required => false
+property :agent_name, :name_attribute => true, :kind_of => String, :required => false
 
-attribute :user, :kind_of => String, :required => false, :default => 'go'
-attribute :group, :kind_of => String, :required => false, :default => 'go'
-attribute :go_server_host, :kind_of => String, :required => false, :default => nil
-attribute :go_server_port, :kind_of => Integer, :required => false, :default => node['gocd']['agent']['go_server_port']
-attribute :daemon, :kind_of => [ TrueClass, FalseClass ], :required => false, :default => node['gocd']['agent']['daemon']
-attribute :vnc, :kind_of => [ TrueClass, FalseClass ], :required => false, :default => node['gocd']['agent']['vnc']['enabled']
-attribute :autoregister_key, :kind_of => String, :required => false, :default => nil
-attribute :autoregister_hostname, :kind_of => String, :required => false, :default => nil
-attribute :environments, :kind_of => [ String, Array ], :required => false, :default => nil
-attribute :resources, :kind_of => [ String, Array ], :required => false, :default => nil
-attribute :workspace, :kind_of => String, :required => false, :default => nil
+property :user, :kind_of => String, :required => false, :default => 'go'
+property :group, :kind_of => String, :required => false, :default => 'go'
+property :go_server_host, :kind_of => String, :required => false, :default => nil
+property :go_server_port, :kind_of => Integer, :required => false, :default => node['gocd']['agent']['go_server_port']
+property :daemon, :kind_of => [ TrueClass, FalseClass ], :required => false, :default => node['gocd']['agent']['daemon']
+property :vnc, :kind_of => [ TrueClass, FalseClass ], :required => false, :default => node['gocd']['agent']['vnc']['enabled']
+property :autoregister_key, :kind_of => String, :required => false, :default => nil
+property :autoregister_hostname, :kind_of => String, :required => false, :default => nil
+property :environments, :kind_of => [ String, Array ], :required => false, :default => nil
+property :resources, :kind_of => [ String, Array ], :required => false, :default => nil
+property :workspace, :kind_of => String, :required => false, :default => nil
+
+action :create do
+  include_recipe 'gocd::agent_linux_install'
+
+  agent_name = new_resource.agent_name
+  workspace = new_resource.workspace || "/var/lib/#{agent_name}"
+  [workspace, "/var/log/#{agent_name}"].each do |d|
+    directory d do
+      mode     0755
+      owner    new_resource.user
+      group    new_resource.group
+    end
+  end
+  directory "#{workspace}/config" do
+    mode     0700
+    owner    new_resource.user
+    group    new_resource.group
+  end
+  # package manages the init.d/go-agent script so cookbook should not.
+  bash "setup init.d for #{agent_name}" do
+    code <<-EOH
+    cp /etc/init.d/go-agent /etc/init.d/#{agent_name}
+    sed -i 's/# Provides: go-agent$/# Provides: #{agent_name}/g' /etc/init.d/#{agent_name}
+    EOH
+    not_if "grep -q '# Provides: #{agent_name}$' /etc/init.d/#{agent_name}"
+    only_if { agent_name != 'go-agent' }
+  end
+  link "/usr/share/#{agent_name}" do
+    to "/usr/share/go-agent"
+    not_if { agent_name == 'go-agent' }
+  end
+
+  autoregister_values = get_agent_properties
+  autoregister_values[:go_server_host] = new_resource.go_server_host || autoregister_values[:go_server_host]
+  autoregister_values[:go_server_port] = new_resource.go_server_port || autoregister_values[:go_server_port]
+  autoregister_values[:key] =  new_resource.autoregister_key || autoregister_values[:key]
+  autoregister_values[:hostname] = new_resource.autoregister_hostname || autoregister_values[:hostname]
+  autoregister_values[:environments] = new_resource.environments || autoregister_values[:environments]
+  autoregister_values[:resources] = new_resource.resources || autoregister_values[:resources]
+  autoregister_values[:vnc] = new_resource.vnc || autoregister_values[:vnc]
+  autoregister_values[:daemon] = new_resource.daemon || autoregister_values[:daemon]
+  autoregister_values[:workspace] = workspace
+  if autoregister_values[:go_server_host].nil?
+    autoregister_values[:go_server_host] = '127.0.0.1'
+    Chef::Log.warn("Go server not found on Chef server or not specifed via node['gocd']['agent']['go_server_host'] attribute, defaulting Go server to #{autoregister_values[:go_server_host]}")
+  end
+
+  template "/etc/default/#{agent_name}" do
+    source   "go-agent-default.erb"
+    cookbook 'gocd'
+    mode     "0644"
+    owner    "root"
+    group    "root"
+    notifies :restart,      "service[#{agent_name}]" if autoregister_values[:daemon]
+    variables autoregister_values
+  end
+
+  if autoregister_values[:key]
+    template "#{workspace}/config/autoregister.properties" do
+      source  'autoregister.properties.erb'
+      cookbook 'gocd'
+      mode     "0644"
+      owner    new_resource.user
+      group    new_resource.group
+      not_if { ::File.exists? ("#{workspace}/config/agent.jks") }
+      notifies :restart, "service[#{agent_name}]" if autoregister_values[:daemon]
+      variables autoregister_values
+    end
+  end
+
+  service agent_name do
+    supports :status => true, :restart => autoregister_values[:daemon], :start => true, :stop => true
+    action   new_resource.service_action
+  end
+end
--- resources/plugin.rb
+++ resources/plugin.rb
@@ -2,6 +2,21 @@ actions :create, :delete
 
 default_action :create if defined?(default_action)
 
-attribute :plugin_name, :name_attribute => true, :kind_of => String, :required => true
-attribute :plugin_uri, :kind_of => String, :required => true
-attribute :server_work_dir, :kind_of => String, :required => false, :default => node['gocd']['server']['work_dir']
+property :plugin_name, :name_attribute => true, :kind_of => String, :required => true
+property :plugin_uri, :kind_of => String, :required => true
+property :server_work_dir, :kind_of => String, :required => false, :default => node['gocd']['server']['work_dir']
+
+action :create do
+  include_recipe 'gocd::server_linux_install'
+
+  plugin_name = new_resource.plugin_name
+  server_work_dir = new_resource.server_work_dir
+  plugin_uri = new_resource.plugin_uri
+  remote_file "#{server_work_dir}/plugins/external/#{plugin_name}.jar" do
+    source plugin_uri
+    owner 'go'
+    group 'go'
+    mode 0770
+    retries 5
+  end
+end
